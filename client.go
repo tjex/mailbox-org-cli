77ab5312127f186003f7ec1a09d1b03c483be58e
tjex <tjex@tjex.net>
2023-09-07 23:04:45 +0200

    structure of RenewAll functional. But not working

--- client.go
+++ client.go
@@ -104,20 +104,23 @@ func (c *Client) Renew(id string) (Address, error) {
 	return c.findAddressByID(id), nil
 }
 
-func (c *Client) RenewAll(id string) (Address, error) {
+func (c *Client) RenewAll() ([]Address, error) {
 	// get all emails and fill into a slice
 	emails := []string{}
 	for _, address := range c.List() {
 		emails = append(emails, address.Email)
 	}
-	// executeAction renew for each email
 
-	err := c.executeAction(FormPayload{"action": "renew", "id": id})
-	if err != nil {
-		return Address{}, err
+	// executeAction renew for each email
+	for _, email := range emails {
+		err := c.executeAction(FormPayload{"action": "renew", "id": email})
+		if err != nil {
+			return c.List(), err
+		}
 	}
 
-	return c.findAddressByID(id), nil
+	return c.List(), nil
+
 }
 
 func (c *Client) SetMemo(id, memo string) (Address, error) {
